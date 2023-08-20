6760a8b01e2b66c406b3c33ff53e7d79866c0c73
create-more <createmoreio@gmail.com>
2023-08-20 22:16:29 +0100

    begin mui5 integration

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,7 +1,9 @@
-`use client`
+import Button from '@mui/material/Button';
 
 export default function LandingPage() {
     return (
-        <div>LANDING PAGE</div>
+        <div>LANDING PAGE
+            <Button variant="contained">Hello world</Button>
+        </div>
     );
 };
